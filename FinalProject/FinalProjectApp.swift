cd6bddbae1560a08d51f99b5b2008fbe1b936b6e
Iroko1 <totokori1@gmail.com>
2025-04-28 09:05:01 -0500

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by ROTSEVENKOV, MATVEY V. on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
