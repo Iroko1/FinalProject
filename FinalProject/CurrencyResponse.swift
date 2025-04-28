35ef2e60595e6e2aaa41cb74d4e60d8cd64c53fa
Iroko1 <totokori1@gmail.com>
2025-04-28 09:35:45 -0500

    innit commit

---
+++ FinalProject/CurrencyResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyResponse.swift
+//  FinalProject
+//
+//  Created by ROTSEVENKOV, MATVEY V. on 4/28/25.
+//
+
+import SwiftUI
+import Foundation
+
+struct CurrencyResponse: Codable {
+    let rates: [String: Double]
+}
